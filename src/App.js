f2948acf97a0927955768fc3d5a6f32ebd988524
mgmg781993 <myatnoe.aidma@gmail.com>
2018-03-29 14:35:57 +0630

    first

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-date-picker';
+var DatePicker = require("react-bootstrap-date-picker");
+
+class App extends Component {
+  getInitialState: function(){
+    var value = new Date().toISOString();
+    return {
+      value: value
+    }
+  },
+  handleChange: function(value, formattedValue) {
+    this.setState({
+      value: value, // ISO String, ex: "2016-11-19T12:00:00.000Z"
+      formattedValue: formattedValue // Formatted String, ex: "11/19/2016"
+    });
+  },
+  componentDidUpdate: function(){
+    // Access ISO String and formatted values from the DOM.
+    var hiddenInputElement = document.getElementById("example-datepicker");
+    console.log(hiddenInputElement.value); // ISO String, ex: "2016-11-19T12:00:00.000Z"
+    console.log(hiddenInputElement.getAttribute('data-formattedvalue')) // Formatted String, ex: "11/19/2016"
+  },
+
+  render() {
+    return (
+      <div>
+         <DatePicker id="example-datepicker" value={this.state.value} onChange={this.handleChange} />
+       </div>
+    );
+  }
+}
+
+export default App;
